af77c12f2a2354bc4e39b9e8cb296b602dd8961b
Görkem Gül <104680758+gorkemgul@users.noreply.github.com>
2023-07-10 23:01:30 +0300

    simple gan implemented with pytorch

--- gan.py
+++ gan.py
@@ -6,7 +6,6 @@ from torch.utils.data import DataLoader
 import torchvision.transforms as transforms
 from torch.utils.tensorboard import SummaryWriter
 
-
 class Discriminator(nn.Module):
     def __init__(self, img_shape: int, output_shape: int = 1):
         super().__init__()
@@ -23,7 +22,7 @@ class Discriminator(nn.Module):
 
 class Generator(nn.Module):
     def __init__(self, latent_dim: int, img_shape: int):
-        super().__init()
+        super().__init__()
         self.generator = nn.Sequential(
             nn.Linear(latent_dim, 256),
             nn.LeakyReLU(0.1),
@@ -70,3 +69,41 @@ criterion = nn.BCELoss()
 writer_fake = SummaryWriter(f"runs/GAN_MNIST/fake")
 writer_real = SummaryWriter(f"runs/GAN_MNIST/real")
 step = 0
+
+
+for epoch in range(epochs):
+    for batch, (real, _) in enumerate(data_loader):
+        real = real.view((-1, 784)).to(device)
+        batch_size = real.shape[0]
+
+        # Train Discriminator: max log(D(x)) + log(1 - D(G(z)))
+        noise = torch.randn(batch_size, latent_dim).to(device)
+        fake = generator(noise)
+        discriminator_real = discriminator(real).view(-1)
+        loss_disc_real = criterion(discriminator_real, torch.ones_like(discriminator_real))
+        discriminator_fake = discriminator(fake)
+        loss_disc_fake = criterion(discriminator_fake, torch.zeros_like(discriminator_fake))
+        loss_disc = (loss_disc_real + loss_disc_fake) / 2
+        discriminator.zero_grad()
+        loss_disc.backward(retain_graph = True)
+        optimizer_disc.step()
+
+        #  Train Generator: min log(1 - D(G(z))) <-> max log(D(G(z))
+        output = discriminator(fake).view(-1)
+        loss_gen = criterion(output, torch.ones_like(output))
+        generator.zero_grad()
+        loss_gen.backward()
+        optimizer_gen.step()
+
+        if batch == 0:
+            print(f"[Epoch {epoch} / {epochs}] \nDiscriminator Loss: {loss_disc:.4f}, Generator Loss: {loss_gen:.4f}")
+
+            with torch.no_grad():
+                fake = generator(fixed_noise).reshape(-1, 1, 28, 28)
+                data = real.reshape(-1, 1, 28, 28)
+                img_grid_fake = torchvision.utils.make_grid(fake, normalize = True)
+                img_grid_real = torchvision.utils.make_grid(data, normalize = True)
+            
+                writer_fake.add_image("MNIST Fake Images", img_grid_fake, global_step = step)
+                writer_real.add_image("MNIST Real Image", img_grid_real, global_step = step)
+                step += 1
