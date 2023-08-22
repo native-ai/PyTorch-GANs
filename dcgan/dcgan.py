266a957b1d6e101cbeb523d25fc15bd23e656cab
Görkem Gül <104680758+gorkemgul@users.noreply.github.com>
2023-08-22 23:14:02 +0300

    add Generator class

--- dcgan/dcgan.py
+++ dcgan/dcgan.py
@@ -21,4 +21,23 @@ class Discriminator(nn.Module):
             nn.LeakyReLU(0.2), 
         )
 
+class Generator(nn.Module):
+    def __init__(self, latent_dim, channels_img, featurs_g):
+        super().__init__()
+        self.generator = nn.Sequential(
+            self._block(in_channels = latent_dim, features_g * 16, 4, 1, 0),
+            self._block(features_g * 16, features_g * 8, 4, 2, 1),
+            self._block(features_g * 8, fatures_g * 4, 4, 2, 1),
+            self._block(features_g * 4, features_g * 2, 4, 2, 1),
+            nn.ConvTranspose2d(features_g * 2, channels_img, kernel_size = 4, stride = 2, padding = 1),
+            nn.Tanh()
+        )
+
+    def _block(self, in_channels, out_channls, kernel_size, stride, padding):
+        return nn.Sequential(
+            nn.ConvTranspose2d(in_channels = in_channels, out_channels = out_channels, kernel_size = kernel_size, stride = stride, padding = padding, bias = False),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU()
+        )
+    
  
