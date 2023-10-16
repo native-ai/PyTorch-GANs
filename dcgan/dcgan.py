e38df5710df93de4d39f7ec8f6023d3f0beb012c
Görkem Gül <104680758+gorkemgul@users.noreply.github.com>
2023-10-16 23:10:36 +0300

    update dcgan model

--- dcgan/dcgan.py
+++ dcgan/dcgan.py
@@ -1,5 +1,10 @@
 import torch 
 import torch.nn as nn 
+import torch.optim as optim
+import torchvision.datasets as datasets
+import torchvision.transforms as transforms
+from torch.utils.data import DataLoader
+from torch.utils.tensorboard import SummaryWriter
 
 class Discriminator(nn.Module):
     def __init__(self, img_shape, features_d):
@@ -54,20 +59,52 @@ def initialize_weights(model):
     for weight in model.modules():
         if isinstance(weight, (nn.Conv2d, nn.ConvTranspose2d, nn.BatchNorm2d)):
             nn.init.normal_(weight.weight.data, 0.0, 0.02)
+            
+            
+device = "cuda" if torch.cuda.is_available() else "cpu"
+LEARNING_RATE = 2e-4
+BATCH_SIZE = 128
+IMAGE_SIZE = 64
+IMG_SHAPE = 1
+LATENT_DIM = 100
+EPOCHS = 100
+FEATURES_D = 64
+FEATURES_G = 64
+
+transforms = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Resize(IMAGE_SIZE),
+    transforms.Normalize([0.5 for _ in range(IMG_SHAPE)], [0.5 for _ in range(IMG_SHAPE)])
+])
+
+dataset = datasets.MNIST(root = "dataset", train = True, transform = transforms, download = True)
+data_loader = DataLoader(dataset, BATCH_SIZE, shuffle = True)
+
+generator = Generator(LATENT_DIM, IMG_SHAPE, FEATURES_G).to(device)
+discriminator = Discriminator(IMG_SHAPE, FEATURES_D).to(device)
+initialize_weights(generator)
+initialize_weights(discriminator)
+optimizer_gen = optim.Adam(generator.parameters(), lr = LEARNING_RATE, betas = (0.5, 0.999))
+optimizer_disc = optim.Adam(discriminator.parameters(), lr = LEARNING_RATE, beats = (0.5, 0.999))
+
+fixed_noise = torch.randn(32, LATENT_DIM, 1, 1).to(device)
 
 
-def test_model():
-    N, in_channels, Height, Width = 8, 3, 64, 64
-    latent_dim = 100
-    x = torch.randn((N, in_channels, Height, Width))
-    discriminator = Discriminator(in_channels, 8)
-    initialize_weights(discriminator)
-    assert discriminator(x).shape == (N, 1, 1, 1)
-    y = torch.randn((N, latent_dim, 1, 1)) 
-    generator = Generator(latent_dim, in_channels, 8)
-    initialize_weights(generator)
-    assert generator(y).shape == (N, in_channels, Height, Width)
-    print("Succesfully finished!")
+
+# def test_model():
+#     N, in_channels, Height, Width = 8, 3, 64, 64
+#     latent_dim = 100
+#     x = torch.randn((N, in_channels, Height, Width))
+#     discriminator = Discriminator(in_channels, 8)
+#     initialize_weights(discriminator)
+#     assert discriminator(x).shape == (N, 1, 1, 1)
+#     y = torch.randn((N, latent_dim, 1, 1)) 
+#     generator = Generator(latent_dim, in_channels, 8)
+#     initialize_weights(generator)
+#     assert generator(y).shape == (N, in_channels, Height, Width)
+#     print("Succesfully finished!")
     
-if __name__ == "__main__":
-    test_model()            
+# if __name__ == "__main__":
+#     test_model()            
+
+
