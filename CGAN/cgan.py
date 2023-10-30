ef8f0dfe0b9487601c540fc0c25a45396e47649e
Görkem Gül <104680758+gorkemgul@users.noreply.github.com>
2023-10-30 22:47:04 +0300

    add gradient penalty functionality to cgan model

--- CGAN/cgan.py
+++ CGAN/cgan.py
@@ -56,3 +56,23 @@ class Generator(nn.Module):
         embedding = self.embed(labels).unsqueeze(2).unsqueeze(3)
         x = torch.cat([x, embedding], dim = 1)
         return self.net(x)
+
+
+def initialize_weights(model):
+    for m in model.modules():
+        if isinstance(m, (nn.Conv2d, nn.ConvTranspose2d, nn.BatchNorm2d)):
+            nn.init.normal_(m.weight.data, 0.0, 0.02)
+
+def gradient_penalty(discriminator, labels, real, fake, device = "cpu"):
+
+    BATCH_SIZE, Channels, Height, Width = real.shape
+    epsilon = torch.rand((BATCH_SIZE, 1, 1, 1)).repeat(1, Channels, Height, Width).to(device)
+    interpolated_images = real * epsilon + fake * (1 - epsilon) # 10 percent of the real image and 90 percent of the fake images
+    mixed_scores = discriminator(interpolated_images, labels)
+    gradient = torch.autograd.grad(inputs = interpolated_images, outputs = mixed_scores, grad_outputs = torch.ones_like(mixed_scores), create_graph = True, retain_graph = True)[0]
+    gradient = gradient.view(gradient.shape[0], -1)
+    gradient_norm = gradient.norm(2, dim = 1)
+    gradient_penalty = torch.mean((gradient_norm - 1) ** 2)
+    return gradient_penalty
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
